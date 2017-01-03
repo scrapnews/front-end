21494bcb9d331bff4ad6cae9592b961ac5f7cd5c
scrapnews <scrapnews514@gmail.com>
2017-01-03 17:12:07 -0500

    change login

---
+++ src/app/scrap-login/scrap-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scrap-login',
+  templateUrl: './scrap-login.component.html',
+  styleUrls: ['./scrap-login.component.css']
+})
+export class ScrapLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
